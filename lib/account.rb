01914c37c69d0fd3b262313b1a7c49d76317ea33
Anna Lucking <annavlucking@hotmail.co.uk>
2017-02-08 13:24:23 +0000

    add withdrawal functionality and printing of transactions

--- lib/account.rb
+++ lib/account.rb
@@ -9,13 +9,24 @@ class Account
     @transactions = []
   end
 
-  def show_balance
-    @balance
-  end
-
   def add_deposit(deposit)
     @balance += deposit.amount
     @transactions << {"date" => deposit.date, "deposit" => deposit.amount, "withdrawal" => 0, "balance" => @balance}
+  end
+
+  def make_withdrawal(withdrawal)
+    @balance -= withdrawal.amount
+    @transactions << {"date" => withdrawal.date, "deposit" => 0, "withdrawal" => withdrawal.amount, "balance" => @balance}
+  end
+
+  def print_transactions
+    puts "Transactions".center(50)
+    @transactions.each do |hash|
+      puts "-----------------".center(50)
+        hash.each do |k, v|
+        puts "#{k}: #{v}".center(50)
+      end
+    end
   end
 
 end
